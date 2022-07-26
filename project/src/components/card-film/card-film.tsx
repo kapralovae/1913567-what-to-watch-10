a208b6aa3ff26a176721fef57677b5ae55a4b442
kapralovae <kapralov.ae@bk.ru>
2022-07-27 00:21:48 +0300

    функия на focus and blur

--- project/src/components/card-film/card-film.tsx
+++ project/src/components/card-film/card-film.tsx
@@ -10,17 +10,13 @@ function CardFilm ({item}: CardType): JSX.Element{
   const {src, title, id, video} = item;
   const [isFosued, setIsFocused] = useState(false);
 
-  const focusHandler = () => {
-    setIsFocused(!isFosued);
-  };
-
-  const blurHandler = () => {
+  const focusAndBlurHandler = () => {
     setIsFocused(!isFosued);
   };
 
   return(
     <article className="small-film-card catalog__films-card">
-      <div onMouseOver={focusHandler} onMouseOut={blurHandler} id={`${id}`} className="small-film-card__image">
+      <div onMouseOver={focusAndBlurHandler} onMouseOut={focusAndBlurHandler} id={`${id}`} className="small-film-card__image">
         {isFosued ? <VideoPlayer video={video}></VideoPlayer> : <img src={src} alt={title} width="280" height="175" />}
       </div>
       <h3 className="small-film-card__title">
